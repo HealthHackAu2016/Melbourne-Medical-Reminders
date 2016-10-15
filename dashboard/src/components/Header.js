80277f6f2234c18fd1165e33c98e10581fb3b47f
PakkuDon <pakku.pd@gmail.com>
2016-10-15 17:16:30 +1100

    Minor UI / style changes
    - Logo links to home page
    - Patient stubs link to profile page
    - Some padding, margin, colour adjustments

--- dashboard/src/components/Header.js
+++ dashboard/src/components/Header.js
@@ -1,10 +1,11 @@
 import React from 'react';
+import { Link } from 'react-router';
 
 export default class Header extends React.Component {
   render() {
     return (
       <header>
-        <img src='src/images/logo.png' alt='lorne community hospital logo' />
+        <Link to='/'><img src='src/images/logo.png' alt='lorne community hospital logo' /></Link>
         <h1>Dashboard - Medical Reminders</h1>
       </header>
     );
--- dashboard/src/components/PatientStub.js
+++ dashboard/src/components/PatientStub.js
@@ -5,28 +5,29 @@ export default class PatientStub extends React.Component {
   render() {
     var patient = this.props.patient;
     return (
-      <div className='patient-stub row'>
-        <div className='patient-photo col-xs-3 col-md-1'>
-          <img src={patient.photo} alt='patient photo' />
+      <Link to={'/patient/' + patient._id}>
+        <div className='patient-stub row'>
+          <div className='patient-photo col-xs-3 col-md-1'>
+            <img src={patient.photo} alt='patient photo' />
+          </div>
+          <div className='col-xs-9 col-md-11'>
+            <dl className='dl-horizontal'>
+              <dt>Name</dt>
+              <dd>{patient.name}</dd>
+              <dt>Phone no.</dt>
+              <dd>{patient.phone_no}</dd>
+              <dt>Permissions</dt>
+              <dd>
+                {
+                  patient.permissions.map((p) => {
+                    return p.content
+                  }).join(', ')
+                }
+              </dd>
+            </dl>
+          </div>
         </div>
-        <div className='col-xs-9 col-md-11'>
-          <dl className='dl-horizontal'>
-            <dt>Name</dt>
-            <dd>{patient.name}</dd>
-            <dt>Phone no.</dt>
-            <dd>{patient.phone_no}</dd>
-            <dt>Permissions</dt>
-            <dd>
-              {
-                patient.permissions.map((p) => {
-                  return p.content
-                }).join(', ')
-              }
-            </dd>
-          </dl>
-          <Link to={'/patient/' + patient._id}>View profile</Link>
-        </div>
-      </div>
+      </Link>
     );
   }
 }
