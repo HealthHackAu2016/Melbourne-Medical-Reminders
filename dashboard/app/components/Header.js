65d944531101372e169361d8ef62ae4687d72b34
PakkuDon <pakku.pd@gmail.com>
2016-10-17 09:39:35 +1100

    Fix links to static assets

--- dashboard/app/components/Header.js
+++ dashboard/app/components/Header.js
@@ -5,7 +5,7 @@ export default class Header extends React.Component {
   render() {
     return (
       <header>
-        <Link to='/'><img src='src/images/logo.png' alt='lorne community hospital logo' /></Link>
+        <Link to='/'><img src='images/logo.png' alt='lorne community hospital logo' /></Link>
         <h1>Dashboard - Medical Reminders</h1>
       </header>
     );
